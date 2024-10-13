c09de1f8c72cc5daf5c7ca8e343091f134681856
harish <harish.venkat2018@gmail.com>
2024-10-14 00:23:53 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./components/Header";
+import { AddTasks } from "./components/AddTasks";
+import { ShowTasks } from "./components/ShowTasks";
+
+import "./App.css";
 
 function App() {
+  // let obj = { key: "value" };
+  // localStorage.setItem("myObject", JSON.stringify(obj));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <AddTasks />
+      <ShowTasks />
+      <h1>Hello</h1>
     </div>
   );
 }
---
+++ src/components/AddTasks.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AddTasks = () => {
+  return <div>AddTasks</div>;
+};
